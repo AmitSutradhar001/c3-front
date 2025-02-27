e456c637785a418b7821e38ab5beb08943f8ef73
AmitSutradhar001 <sutradharamit001@gmail.com>
2025-02-27 22:57:26 +0530

    ds

--- src/pages/Applyed.jsx
+++ src/pages/Applyed.jsx
@@ -124,6 +124,7 @@ const Applyed = () => {
   return (
     <>
       {pData && <p>amit</p>}
+      {pData && <p>{pData.content}</p>}
       {pData && (
         <div className="ad-outer">
           <div className="ad-m-1">
